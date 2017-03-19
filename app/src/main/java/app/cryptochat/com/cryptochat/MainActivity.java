dbbbf2fb3feae94109b5aa87a1a80e8b3b3b45dd
Sergey Romankov <romankov.sergey@gmail.com>
2017-03-19 11:33:14 +0300

    init

---
+++ app/src/main/java/app/cryptochat/com/cryptochat/MainActivity.java
@@ -0,0 +1,13 @@
+package app.cryptochat.com.cryptochat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
